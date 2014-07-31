652bd692addd19024839e90dce45e7b55a090ab1
James Tauber <jtauber@jtauber.com>
2014-07-31 15:14:31 -0400

    fixed long description reference to README filename

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.md"),
+    long_description=read("README.rst"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="BSD",
